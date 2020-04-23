c87fe72e151ebc9deea1b2f39487f575015894e0
maxep <me@maxep.me>
2020-04-23 11:55:37 +0200

    Version 0.2.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core')
 const exec = require('@actions/exec')
 
 async function main() {
-    await exec.exec('sh cov.sh')
+    await exec.exec(`${__dirname}/cov.sh`)
 }
 
 main().catch(err => core.setFailed(err.message))
